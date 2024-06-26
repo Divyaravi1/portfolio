3548f26466f779c2b2d4c9f777d87b02151a2b96
drav2595 <drav2595@uni.sydney.edu.au>
2024-06-26 21:39:10 +1000

    Beach + tsg updates

--- ux-projects/assets/js/main.js
+++ ux-projects/assets/js/main.js
@@ -299,3 +299,64 @@ const lightboxImg = document.createElement('img');
     });
     });
 
+
+
+    /* flip cards */
+
+    function toggleFlip(card) {
+      card.querySelector('.flip-card-inner').classList.toggle('flipped');
+    }
+
+    /*Tooltip*/ 
+
+    $('.circle').tooltip('show')
+    $(function () {
+      $('[data-toggle="tooltip"]').tooltip()
+    });
+    
+
+
+
+    document.addEventListener("DOMContentLoaded", function() {
+      window.addEventListener("scroll", reveal);
+    
+      function reveal() {
+        var reveals = document.querySelectorAll(".slide");
+    
+        for (var i = 0; i < reveals.length; i++) {
+          var windowHeight = window.innerHeight;
+          var revealTop = reveals[i].getBoundingClientRect().top;
+          var revealPoint = 50;
+    
+          if (revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add("active");
+          }
+        }
+      }
+    });
+    
+    /*
+    var slideIndex = 1;
+  showSlides(slideIndex);
+
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex-1].style.display = "block";  
+  }
+*/
